ad6f9d25199020b1acbf1be27af8651d6e0f23e7
AugmentedDuck <anders.m.matzen@gmail.com>
2025-05-10 13:00:37 +0200

    Changed .log to .warn at warnings

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ for (const folder of commandFolders) {
             client.commands.set(command.data.name, command);
         }
         else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            console.warn(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
     }
 }
