11f21baf66f7f95073bc00213450d7f99b452157
AugmentedDuck <anders.m.matzen@gmail.com>
2025-09-03 19:19:57 +0200

    Added a is it friday command
    To actually do what it says on the box

---
+++ commands/fun/friday.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('friday')
+        .setDescription('Is it friday?'),
+
+// ////////////////////////////////////
+//
+// Execute function
+//
+// ////////////////////////////////////
+    async execute(interaction) {
+        const today = new Date();
+        const isFriday = today.getDay() === 5;
+
+        await interaction.reply(`${isFriday ? 'https://tenor.com/view/friday-stop-working-start-drinking-gif-16613439415718644570' : 'No\nhttps://tenor.com/view/kpop-looking-for-friday-gif-11705491'}`);
+    },
+};
